ee5ed3ef8dc6859166b1ce6b3fb327ac2a7f6f48
psegarel <patrick@ps.dev>
2013-06-15 14:06:30 +0800

    Cleanup

---
+++ legacy_files/advagg_js/js_3d7ac1d65daee33a440c58858188c945_0.js
@@ -0,0 +1,4 @@
+Drupal.behaviors.textarea=function(context){$('textarea.resizable:not(.textarea-processed)',context).each(function(){if($(this).is('textarea.teaser:not(.teaser-processed)'))return false;var textarea=$(this).addClass('textarea-processed'),staticOffset=null;$(this).wrap('<div class="resizable-textarea"><span></span></div>').parent().append($('<div class="grippie"></div>').mousedown(startDrag));var grippie=$('div.grippie',$(this).parent())[0];grippie.style.marginRight=(grippie.offsetWidth-$(this)[0].offsetWidth)+'px'
+function startDrag(e){staticOffset=textarea.height()-e.pageY;textarea.css('opacity',0.25);$(document).mousemove(performDrag).mouseup(endDrag);return false}
+function performDrag(e){textarea.height(Math.max(32,staticOffset+e.pageY)+'px');return false}
+function endDrag(e){$(document).unbind("mousemove",performDrag).unbind("mouseup",endDrag);textarea.css('opacity',1)}})};
